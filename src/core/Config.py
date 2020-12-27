3760e9eb047df7bf03081683bf8b7027935e6564
Juan Pablo Aguirre <juanpablomartinez590@hotmail.com>
2020-12-27 02:01:37 +0100

    runtime config structure added

---
+++ src/core/Config.py
@@ -0,0 +1,11 @@
+class Config:
+
+    path = None
+
+    dir_web_source = "C:\\Users\\Juan Pablo\\Desktop\\datalake\\TD_DATALAKE\\DATALAKE\\0_SOURCE_WEB"
+    dir_landing_zone = "C:\\Users\\Juan Pablo\\Desktop\\datalake\\TD_DATALAKE\\DATALAKE\\1_LANDING_ZONE"
+    dir_curated_zone = "C:\\Users\\Juan Pablo\\Desktop\\datalake\\TD_DATALAKE\\DATALAKE\\2_CURATED_ZONE"
+    dir_logger_output = "C:\\Users\\Juan Pablo\\Desktop\\datalake\\TD_DATALAKE\\LOGFILES"
+
+    entities = []
+    platforms = []
