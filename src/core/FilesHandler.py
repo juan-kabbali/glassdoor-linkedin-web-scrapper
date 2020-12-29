e2816b1651f4fc57e0c5c3ab656fb3ef74bc673c
Juan Pablo Aguirre <juanpablomartinez590@hotmail.com>
2020-12-29 22:32:22 +0100

    loadFile method added

--- src/core/FilesHandler.py
+++ src/core/FilesHandler.py
@@ -1,3 +1,4 @@
+import codecs
 import glob
 import loguru
 
@@ -7,3 +8,7 @@ def list_files(path, recursive=False):
     return glob.glob(path, recursive=recursive)
 
 
+def load_file(file_path):
+    with open(file_path, "r", encoding="utf8") as file:
+        loguru.logger.trace("Opening file at {file_path}", file_path=file_path)
+        return file.read()
