6db5289bbac138296f2609c0742d274e591d269b
Juan Pablo Aguirre <juanpablomartinez590@hotmail.com>
2020-12-29 22:31:51 +0100

    analyse method from scrapper to call dynamically the parsers

--- src/scrapper.py
+++ src/scrapper.py
@@ -2,14 +2,21 @@ import os
 import re
 import shutil
 import sys
+from datetime import datetime
+from random import randrange
+
 import click
 import loguru
 import setup
-
+import pandas
 
 from src.commands.LoadConfigCommand import LoadConfigFileCommand
 from src.core import Logger, FilesHandler
 from src.core.Config import Config
+from src.parsers.BaseParser import BaseParser
+from src.parsers.EnterpriseReviewParser import EnterpriseReviewParser
+from src.parsers.EnterpriseParser import EnterpriseParser
+from src.parsers.JobParser import JobParser
 
 
 @click.command()
@@ -64,4 +71,32 @@ def classify():
 
 def analyse():
     loguru.logger.info("ANALYZE COMMAND CALLED")
-    pass
+    for platform in Config.platforms:
+        path = os.path.join(Config.dirs['landing_zone'], Config.platforms[platform]['path_name'])
+        loguru.logger.trace("Analyzing {platform} landing zone at {path}", platform=platform.upper(), path=path)
+
+        for entity in Config.entities:
+            path = os.path.join(path, Config.entities[entity]['path_name'])
+            loguru.logger.trace("Analyzing {entity} entity at {path}", entity=entity.upper(), path=path)
+            files = FilesHandler.list_files(os.path.join(path, "*.html"))
+            parser = globals()[Config.entities[entity]['parser']]
+            parser_instance = parser()
+
+            for file in files:
+                parser_instance.file = file
+                parser_instance.parse()
+                parser_instance.extract_metadata()
+
+            # write csv file with retrieved data
+            csv_path = os.path.join(*[Config.dirs['curated_zone'],
+                                      Config.platforms[platform]['path_name'],
+                                      Config.entities[entity]['path_name'],
+                                      f'{entity}.csv'])
+            parser_instance.data_to_csv(csv_path, BaseParser.CSV_EXPORT['DATA'])
+
+            # cleanup last path segment to next iteration
+            path = os.path.dirname(path)
+
+    # write csv file with file's metadata
+    BaseParser.data_to_csv(None, os.path.join(Config.dirs['curated_zone'], 'metadata.csv'),
+                           BaseParser.CSV_EXPORT['METADATA'])
