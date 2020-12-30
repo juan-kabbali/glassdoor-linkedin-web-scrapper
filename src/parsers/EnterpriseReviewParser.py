8778b8a025f57e3ff96df579e0af4a5f888bc486
Juan Pablo Aguirre <juanpablomartinez590@hotmail.com>
2020-12-30 01:17:45 +0100

    enterprise review parser more analysed fields added

--- src/parsers/EnterpriseReviewParser.py
+++ src/parsers/EnterpriseReviewParser.py
@@ -25,18 +25,17 @@ class EnterpriseReviewParser(BaseParser):
         ✅ - score_opportunites
         ✅ - score_remuneration
         ✅ - score_equipe
-           - recommande
-           - point_de_vue
-           - avis_pdg
-
-           - type_employe
-           - poste_employe
-           - ville_employe
-           - intro_avis
-           - avantages_text_avis
-           - inconvenients_text_avis
-           - conseils_text_avis
-           - nombre_utile
+        ✅ - recommande
+        ✅ - point_de_vue
+        ✅ - avis_pdg
+        ✅ - type_employe
+        ✅ - poste_employe
+        ✅ - ville_employe
+        ✅ - intro_avis
+        ✅ - avantages_text_avis
+        ✅ - inconvenients_text_avis
+        ✅ - conseils_text_avis
+        ✅ - nombre_utile
         """
         loguru.logger.trace("Enterprise Review parse method called for file {file}", file=self.file)
 
@@ -61,7 +60,8 @@ class EnterpriseReviewParser(BaseParser):
             try:
                 date = review.find('time')['datetime']
             except TypeError:
-                loguru.logger.warning("file {file} has not '{field_name}' score", file=self.file, field_name=field_name)
+                loguru.logger.warning("One review of file {file} has not '{field_name}' score",
+                                      file=self.file, field_name=field_name)
                 date = None
 
             score = review.find('span', attrs={
@@ -86,8 +86,65 @@ class EnterpriseReviewParser(BaseParser):
                 score_leader_team = review.find('div', text='Équipe dirigeante').next_sibling['title']
 
             except AttributeError:
-                loguru.logger.warning("file {file} has not '{field_name}' score", file=self.file, field_name=field_name)
+                loguru.logger.warning("One review of file {file} has not '{field_name}' score",
+                                      file=self.file, field_name=field_name)
 
+            score_recommend = score_point_of_view = score_pdg = None
+            try:
+                for recommend in review.find('div', attrs={'class': 'recommends'}).find_all('span'):
+                    if 'recommande' in recommend.text.lower():
+                        score_recommend = recommend.text
+
+                    if 'vue' in recommend.text.lower():
+                        score_point_of_view = recommend.text
+
+                    if 'pdg' in recommend.text.lower():
+                        score_pdg = recommend.text
+            except AttributeError:
+                loguru.logger.warning("One review of file {file} has not recommendations score", file=self.file)
+
+            try:
+                employee_city = soup.find('span', attrs={
+                    'class': 'authorLocation'
+                }).text
+            except AttributeError:
+                loguru.logger.warning("One review of file {file} has not employee city", file=self.file)
+                employee_city = None
+
+            employee_type = employee_job = None
+            try:
+                employee_data = soup.find('span', attrs={
+                    'class': 'authorJobTitle'
+                }).text.split('-')
+
+                employee_type = employee_data[0].strip()
+                employee_job = employee_data[1].strip()
+            except IndexError:
+                loguru.logger.warning("One review of file {file} has not complete employee data", file=self.file)
+
+            review_intro = review.find('p', attrs={
+                'class': 'mainText'
+            }).text
+
+            review_advantages = review_disadvantages = review_advises = None
+            try:
+                field_name = 'review_advantages'
+                review_advantages = review.find('p', text="Avantages").next_sibling.text
+
+                field_name = 'review_disadvantages'
+                review_disadvantages = review.find('p', text="Inconvénients").next_sibling.text
+
+                field_name = 'review_advises'
+                review_advises = review.find('p', text="Conseils à la direction").next_sibling.text
+            except AttributeError:
+                loguru.logger.warning("One review of file {file} has not {field_name} data",
+                                      file=self.file, field_name=field_name)
+
+            useful_times = re.sub(r'[^\d]', '', review.find('button', attrs={
+                'class': 'gd-ui-button'
+            }).text).strip()
+
+            useful_times = useful_times if useful_times else 0
 
             row = {
                 'enterprise_name': enterprise_name,
@@ -100,31 +157,21 @@ class EnterpriseReviewParser(BaseParser):
                 'score_opportunities': score_opportunities,
                 'score_wage_advantages': score_wage_advantages,
                 'score_leader_team': score_leader_team,
+                'score_recommend': score_recommend,
+                'score_point_of_view': score_point_of_view,
+                'score_pdg': score_pdg,
+                'employee_type': employee_type,
+                'employee_job': employee_job,
+                'employee_city': employee_city,
+                'review_intro': review_intro,
+                'review_advantages': review_advantages,
+                'review_disadvantages': review_disadvantages,
+                'review_advises': review_advises,
+                'useful_times': useful_times
             }
 
             # add current row to data instance
             self.data.append(row)
-
-        # city = soup.find('span', attrs={
-        #     'class': 'topcard__flavor topcard__flavor--bullet'
-        # }).text
-        # job_description = soup.find('div', attrs={
-        #     'class': 'description__text description__text--rich'
-        # }).text
-        #
-        # hierarchy_level = soup.find('h3', text="Niveau hiérarchique").next_sibling.text
-        # job_type = soup.find('h3', text="Type d’emploi").next_sibling.text
-        #
-        # functions = []
-        # for function in soup.find('h3', text="Fonction").next_siblings:
-        #     functions.append(function.text)
-        #
-        # sectors = []
-        # try:
-        #     for sector in soup.find('h3', text="Secteurs").next_siblings:
-        #         sectors.append(sector.text)
-        # except AttributeError:
-        #     loguru.logger.warning("file {file} has not 'secteurs' job information", file=self.file)
             self.print_row(row)
 
     def extract_metadata(self):
